77afd279be3c34cf195c1c7ad027266795ff116d
jbellegarde <joviane.bellegarde@snowflake.com>
2022-06-13 16:49:07 -0400

    added header, read csv file and generated profile report

---
+++ day14.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+import pandas_profiling
+from streamlit_pandas_profiling import st_profile_report
+
+st.header('`streamlit_pandas_profiling`')
+
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+
+# pandas profiling report is generated
+pr = df.profile_report()
+
+# and displayed
+st_profile_report(pr)
