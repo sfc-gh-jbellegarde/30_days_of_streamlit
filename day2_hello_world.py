05d99307de6431b164942c45195849c30b49a836
Joviane Bellegarde <joviane.bellegarde@snowflake.com>
2024-11-01 10:32:09 -0400

    adding analytics

--- day2_hello_world.py
+++ day2_hello_world.py
@@ -1,2 +1,22 @@
+# Import python packages
 import streamlit as st
-st.write('hello world!')
+import streamlit.components.v1 as components
+
+components.html(
+    """
+    <head>
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-6NXDHHH86G"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-6NXDHHH86G');
+        </script>
+    </head>
+    """,
+    height=0
+)
+
+st.header("Hello World!")
